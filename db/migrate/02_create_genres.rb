559f99fa7488c82576632d1f03dfcca39e3c8596
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2020-09-12 21:32:33 -0700

    Done.

--- db/migrate/02_create_genres.rb
+++ db/migrate/02_create_genres.rb
@@ -1,2 +1,7 @@
-class CreateGenres < ActiveRecord::Migration[4.2]
+class CreateGenres < ActiveRecord::Migration[5.2]
+    def change
+        create_table :genres do |genre|
+            genre.string :name
+        end
+    end
 end
