14d1372d34a2cbe5e2470516eb8bd572ad794e5c
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2020-09-17 10:53:11 -0700

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,11 @@
 class Artist < ActiveRecord::Base
-end
+    has_many :songs
+    has_many :genres, through: :songs
+end 
+
+
+
+
+# ask an Artist about its songs and genres
+# Artists have many songs and a song belongs to an artist
+# Artists have many genres through songs
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,12 @@
 class Genre < ActiveRecord::Base
+    has_many :songs
+    has_many :artists, through: :songs
 end
+
+
+
+
+
+# ask a Genre about its songs and artists
+# A genre has many songs
+# A genre has many artists through songs
